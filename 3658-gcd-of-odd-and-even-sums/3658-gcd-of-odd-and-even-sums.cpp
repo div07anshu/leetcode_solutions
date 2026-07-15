c9f5cbc1015733f2cded6b1ced771f074ee50c7f
Divyanshu <divyanshu202507@gmail.com>
2026-07-15 12:33:41 +0530

    Time: 2 ms (18.99%), Space: 8.4 MB (85.02%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,9 +1,4 @@
 class Solution {
 public:
-    int gcdOfOddEvenSums(int n) {
-        int sumo = (n * (2 + (n - 1) * 2)) / 2;
-        int sume = (n * (4 + (n - 1) * 2)) / 2;
-
-        return gcd(sumo, sume);
-    }
+    int gcdOfOddEvenSums(int n) { return n; }
 };
