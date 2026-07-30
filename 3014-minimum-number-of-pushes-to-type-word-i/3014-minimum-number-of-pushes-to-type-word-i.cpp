a346e997121c5715ad15b2501c6d8f93167c76d0
Divyanshu <divyanshu202507@gmail.com>
2026-07-31 01:04:26 +0530

    Time: 0 ms (100%), Space: 8.2 MB (77.73%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -3,21 +3,13 @@ public:
     int minimumPushes(string word) {
         int n = word.size();
         int ans = 0;
-        map<char, int> mp;
         int cnt = 0;
 
         for (auto x : word) {
-            mp[x]++;
-        }
-
-        for (auto it : mp) {
             cnt++;
-            char x = it.first;
-            int freq = it.second;
-
-            ans += (cnt / 8 + (cnt % 8 != 0)) * freq;
+            ans += (cnt - 1) / 8 + 1;
         }
 
-        return ans ;
+        return ans;
     }
 };
