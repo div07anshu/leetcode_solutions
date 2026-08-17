81e6a3283b6d4adf6ec0e946c980d220426b857b
Divyanshu <divyanshu202507@gmail.com>
2026-08-17 17:54:22 +0530

    Time: 0 ms (100%), Space: 11.7 MB (14.01%) - LeetHub

---
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> decrypt(vector<int>& code, int k) {
+        int n = code.size();
+        vector<int> ans(n);
+
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+
+            if (k == 0) {
+                code[i] = 0;
+            } else if (k < 0) {
+                int a = -1 * k;
+                for (int j = i - 1; j >= i - a; j--) {
+                    sum += code[(j + n) % n];
+                }
+                ans[i] = sum;
+            } else {
+                for (int j = i + 1; j <= i + k; j++) {
+                    sum += code[(j + n) % n];
+                }
+                ans[i] = sum;
+            }
+        }
+
+        return ans;
+    }
+};
